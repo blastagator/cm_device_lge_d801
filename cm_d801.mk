7a014de7857ea466650c1bb5138d13e451cbbb9f
Greg Wallace <greg@gregtwallace.com>
2014-12-06 14:47:06 -0500

    Add AndroidProducts. Slight renaming.

--- cm_d801.mk
+++ cm_d801.mk
@@ -12,7 +12,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/lge/d801/d801.mk)
+$(call inherit-product, device/lge/d801/full_d801.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := d801
